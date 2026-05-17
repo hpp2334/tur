90af2c54321eb0a8e02e7e3db8b36904e335264a
hpp2334 <hpp2334@outlook.com>
2026-05-17 21:42:34 +0800

    fix: use Color.hex() in rich-text test cases

--- js/packages/tur-test-cases/react-cases/rich-text-color/index.tsx
+++ js/packages/tur-test-cases/react-cases/rich-text-color/index.tsx
@@ -1,12 +1,12 @@
-import { renderRoot } from "@tur/react-renderer";
+import { renderRoot, Color } from "@tur/react-renderer";
 import { TextContainer, TextSpan } from "@tur/react";
 
 function RichTextColor() {
   return (
     <TextContainer fontSize={14}>
       <TextSpan content="White" />
-      <TextSpan content="Red" color="#ff0000" />
-      <TextSpan content="Green" color="#00ff00" />
+      <TextSpan content="Red" color={Color.hex("#ff0000")} />
+      <TextSpan content="Green" color={Color.hex("#00ff00")} />
     </TextContainer>
   );
 }
--- js/packages/tur-test-cases/react-cases/rich-text-inheritance/index.tsx
+++ js/packages/tur-test-cases/react-cases/rich-text-inheritance/index.tsx
@@ -1,11 +1,11 @@
-import { renderRoot } from "@tur/react-renderer";
+import { renderRoot, Color } from "@tur/react-renderer";
 import { TextContainer, TextSpan } from "@tur/react";
 
 function RichTextInheritance() {
   return (
     <TextContainer fontSize={20}>
-      <TextSpan content="Inherited" color="#ff0000" />
-      <TextSpan content="Override" fontSize={10} color="#00ff00" />
+      <TextSpan content="Inherited" color={Color.hex("#ff0000")} />
+      <TextSpan content="Override" fontSize={10} color={Color.hex("#00ff00")} />
     </TextContainer>
   );
 }
