1468e899ef99e43933196a7b5ff5d4b40a467854
hpp2334 <hpp2334@outlook.com>
2026-06-21 01:20:01 +0800

    chore: trim demo whitelist to counter and todolist, add libc to integration tests

--- js/packages/tur-demo-impl/src/state/sources.ts
+++ js/packages/tur-demo-impl/src/state/sources.ts
@@ -8,11 +8,7 @@ import type { LayoutMode } from "./types";
 
 const WHITELIST = new Set([
     "counter",
-    "clickable-text",
-    "container-basic",
-    "column-basic",
     "todolist",
-    "multi-file-demo",
 ]);
 
 export const CASE_NAMES = Object.keys(CASE_SOURCES)
