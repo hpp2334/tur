d60aaa5dee3137e7bf5406ffdb36a7586b70c1d8
hpp2334 <hpp2334@outlook.com>
2026-06-21 01:27:04 +0800

    fix: collapse biome formatting for trimmed WHITELIST set

--- js/packages/tur-demo-impl/src/state/sources.ts
+++ js/packages/tur-demo-impl/src/state/sources.ts
@@ -6,10 +6,7 @@ import type { LayoutMode } from "./types";
 // Whitelist & case ordering
 // ---------------------------------------------------------------------------
 
-const WHITELIST = new Set([
-    "counter",
-    "todolist",
-]);
+const WHITELIST = new Set(["counter", "todolist"]);
 
 export const CASE_NAMES = Object.keys(CASE_SOURCES)
     .filter((name) => WHITELIST.has(name))
